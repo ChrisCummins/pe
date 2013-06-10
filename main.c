1306a87fdeb9aa67078f7e0888c670fe0ab3189f
Chris Cummins <christopher.e.cummins@intel.com>
2013-06-10 13:53:20 +0100

    main: Use g_idle callback instead of while(1)

--- main.c
+++ main.c
@@ -8,15 +8,15 @@
 #define HEIGHT 768
 
 struct pe {
+	CoglContext *ctx;
 	CoglFramebuffer *fb;
+	CoglMatrix view;
 	int width, height;
 
-	CoglMatrix view;
 	struct particle_engine *engine[5];
 
 	CoglBool swap_ready;
 	GMainLoop *main_loop;
-	CoglContext *ctx;
 };
 
 static void paint_cb (struct pe *pe) {
@@ -38,21 +38,47 @@ static void frame_event_cb(CoglOnscreen *onscreen, CoglFrameEvent event,
 		pe->swap_ready = TRUE;
 }
 
-int
-main(int argc, char **argv)
 {
 	CoglContext *ctx;
+static gboolean _update_cb(gpointer data)
+{
+	struct pe *pe = data;
+	CoglPollFD *poll_fds;
+	int n_poll_fds;
+	int64_t timeout;
+
+	if (pe->swap_ready) {
+		paint_cb(pe);
+		cogl_onscreen_swap_buffers(COGL_ONSCREEN(pe->fb));
+	}
+
+	cogl_poll_renderer_get_info(cogl_context_get_renderer(pe->ctx),
+				    &poll_fds, &n_poll_fds, &timeout);
+
+	g_poll ((GPollFD *)poll_fds, n_poll_fds,
+		timeout == -1 ? -1 : timeout / 1000);
+
+	cogl_poll_renderer_dispatch(cogl_context_get_renderer(pe->ctx),
+				    poll_fds, n_poll_fds);
+
+	return TRUE;
+}
+
+int main(int argc, char **argv)
+{
+	GMainLoop *loop;
+
 	CoglOnscreen *onscreen;
 	CoglError *error = NULL;
 	struct pe pe;
 	float fovy, aspect, z_near, z_2d, z_far;
 	unsigned int i;
 
-	ctx = cogl_context_new (NULL, &error);
-	if (!ctx || error != NULL)
+	pe.ctx = cogl_context_new (NULL, &error);
+	if (!pe.ctx || error != NULL)
 		g_error("Failed to create Cogl context\n");
 
-	onscreen = cogl_onscreen_new(ctx, WIDTH, HEIGHT);
+	onscreen = cogl_onscreen_new(pe.ctx, WIDTH, HEIGHT);
 
 	pe.fb = COGL_FRAMEBUFFER(onscreen);
 	pe.width = cogl_framebuffer_get_width(pe.fb);
@@ -76,12 +102,16 @@ main(int argc, char **argv)
 
 	cogl_onscreen_add_frame_callback(COGL_ONSCREEN(pe.fb),
 					 frame_event_cb, &pe, NULL);
+	pe.timeout_id = g_timeout_add(5000, _timeout_cb, &pe);
 
 	for (i = 0; i < G_N_ELEMENTS(pe.engine); i++) {
-		pe.engine[i] = particle_engine_new(ctx, pe.fb);
+		pe.engine[i] = particle_engine_new(pe.ctx, pe.fb);
 
 		pe.engine[i]->max_particles = 10000;
 		pe.engine[i]->point_size = 3.0f;
+
+		pe.engine[i]->min_initial_position[1] = (float)HEIGHT + 5;
+		pe.engine[i]->max_initial_position[1] = (float)HEIGHT + 5;
 
 		pe.engine[i]->min_initial_velocity[0] = -50.0f;
 		pe.engine[i]->min_initial_velocity[1] = -600.0f;
@@ -97,40 +127,32 @@ main(int argc, char **argv)
 	pe.engine[0]->max_initial_velocity[1] = -1000.0f;
 
 	pe.engine[0]->min_initial_position[0] = (float)WIDTH / 2 - 5;
-	pe.engine[0]->min_initial_position[1] = (float)HEIGHT;
 	pe.engine[0]->min_initial_position[2] = 0.0f;
 
 	pe.engine[0]->max_initial_position[0] = (float) WIDTH / 2 + 5;
-	pe.engine[0]->max_initial_position[1] = (float)HEIGHT;
 	pe.engine[0]->max_initial_position[2] = 0.0f;
 
 	/* fountain 2 */
 	pe.engine[1]->min_initial_position[0] = (float)WIDTH / 4 - 5;
-	pe.engine[1]->min_initial_position[1] = (float)HEIGHT;
 	pe.engine[1]->min_initial_position[2] = 0.0f;
 
 	pe.engine[1]->max_initial_position[0] = (float) WIDTH / 4 + 5;
-	pe.engine[1]->max_initial_position[1] = (float)HEIGHT;
 	pe.engine[1]->max_initial_position[2] = 0.0f;
 
 	/* fountain 3 */
 	pe.engine[2]->min_initial_position[0] = ((float)WIDTH / 4) * 3 - 5;
-	pe.engine[2]->min_initial_position[1] = (float)HEIGHT;
 	pe.engine[2]->min_initial_position[2] = 0.0f;
 
 	pe.engine[2]->max_initial_position[0] = ((float) WIDTH / 4) * 3 + 5;
-	pe.engine[2]->max_initial_position[1] = (float)HEIGHT;
 	pe.engine[2]->max_initial_position[2] = 0.0f;
 
 	/* fountain 4 */
 	pe.engine[3]->max_particles = 2000;
 
 	pe.engine[3]->min_initial_position[0] = -5.0f;
-	pe.engine[3]->min_initial_position[1] = (float)HEIGHT;
 	pe.engine[3]->min_initial_position[2] = 0.0f;
 
 	pe.engine[3]->max_initial_position[0] = 5.0f;
-	pe.engine[3]->max_initial_position[1] = (float)HEIGHT;
 	pe.engine[3]->max_initial_position[2] = 0.0f;
 
 	pe.engine[3]->min_initial_velocity[0] = 300.0f;
@@ -145,11 +167,9 @@ main(int argc, char **argv)
 	pe.engine[4]->max_particles = 2000;
 
 	pe.engine[4]->min_initial_position[0] = (float)WIDTH -5;
-	pe.engine[4]->min_initial_position[1] = (float)HEIGHT;
 	pe.engine[4]->min_initial_position[2] = 0.0f;
 
 	pe.engine[4]->max_initial_position[0] = (float)WIDTH + 5;
-	pe.engine[4]->max_initial_position[1] = (float)HEIGHT;
 	pe.engine[4]->max_initial_position[2] = 0.0f;
 
 	pe.engine[4]->min_initial_velocity[0] = -400.0f;
@@ -160,25 +180,10 @@ main(int argc, char **argv)
 	pe.engine[4]->max_initial_velocity[1] = -400.0f;
 	pe.engine[4]->max_initial_velocity[2] = 150.0f;
 
-	while (1) {
-		CoglPollFD *poll_fds;
-		int n_poll_fds;
-		int64_t timeout;
+	g_idle_add(_update_cb, &pe);
 
-		if (pe.swap_ready) {
-			paint_cb(&pe);
-			cogl_onscreen_swap_buffers(COGL_ONSCREEN(pe.fb));
-		}
-
-		cogl_poll_renderer_get_info(cogl_context_get_renderer(ctx),
-					    &poll_fds, &n_poll_fds, &timeout);
-
-		g_poll ((GPollFD *)poll_fds, n_poll_fds,
-			timeout == -1 ? -1 : timeout / 1000);
-
-		cogl_poll_renderer_dispatch(cogl_context_get_renderer(ctx),
-					    poll_fds, n_poll_fds);
-	}
+	loop = g_main_loop_new (NULL, TRUE);
+	g_main_loop_run (loop);
 
 	return 0;
 }
