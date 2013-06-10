eac3479e539dfd42d780bb32f7c383afb74f9c73
Chris Cummins <christopher.e.cummins@intel.com>
2013-06-10 11:54:32 +0100

    main: Remove the cube fluff

--- main.c
+++ main.c
@@ -12,59 +12,11 @@ struct pe {
 	int width, height;
 
 	CoglMatrix view;
-	CoglIndices *indices;
-	CoglPrimitive *primitive;
-	CoglTexture *texture;
-	CoglPipeline *cube_pipeline;
-
 	struct particle_engine *engine[5];
 
-	GTimer *timer;
 	CoglBool swap_ready;
 	GMainLoop *main_loop;
 	CoglContext *ctx;
-};
-
-static CoglMatrix identity;
-static CoglColor white;
-
-static CoglVertexP3T2 vertices[] =
-{
-	/* Front face */
-	{ /* pos = */ -1.0f, -1.0f,  1.0f, /* tex coords = */ 0.0f, 1.0f},
-	{ /* pos = */  1.0f, -1.0f,  1.0f, /* tex coords = */ 1.0f, 1.0f},
-	{ /* pos = */  1.0f,  1.0f,  1.0f, /* tex coords = */ 1.0f, 0.0f},
-	{ /* pos = */ -1.0f,  1.0f,  1.0f, /* tex coords = */ 0.0f, 0.0f},
-
-	/* Back face */
-	{ /* pos = */ -1.0f, -1.0f, -1.0f, /* tex coords = */ 1.0f, 0.0f},
-	{ /* pos = */ -1.0f,  1.0f, -1.0f, /* tex coords = */ 1.0f, 1.0f},
-	{ /* pos = */  1.0f,  1.0f, -1.0f, /* tex coords = */ 0.0f, 1.0f},
-	{ /* pos = */  1.0f, -1.0f, -1.0f, /* tex coords = */ 0.0f, 0.0f},
-
-	/* Top face */
-	{ /* pos = */ -1.0f,  1.0f, -1.0f, /* tex coords = */ 0.0f, 1.0f},
-	{ /* pos = */ -1.0f,  1.0f,  1.0f, /* tex coords = */ 0.0f, 0.0f},
-	{ /* pos = */  1.0f,  1.0f,  1.0f, /* tex coords = */ 1.0f, 0.0f},
-	{ /* pos = */  1.0f,  1.0f, -1.0f, /* tex coords = */ 1.0f, 1.0f},
-
-	/* Bottom face */
-	{ /* pos = */ -1.0f, -1.0f, -1.0f, /* tex coords = */ 1.0f, 1.0f},
-	{ /* pos = */  1.0f, -1.0f, -1.0f, /* tex coords = */ 0.0f, 1.0f},
-	{ /* pos = */  1.0f, -1.0f,  1.0f, /* tex coords = */ 0.0f, 0.0f},
-	{ /* pos = */ -1.0f, -1.0f,  1.0f, /* tex coords = */ 1.0f, 0.0f},
-
-	/* Right face */
-	{ /* pos = */ 1.0f, -1.0f, -1.0f, /* tex coords = */ 1.0f, 0.0f},
-	{ /* pos = */ 1.0f,  1.0f, -1.0f, /* tex coords = */ 1.0f, 1.0f},
-	{ /* pos = */ 1.0f,  1.0f,  1.0f, /* tex coords = */ 0.0f, 1.0f},
-	{ /* pos = */ 1.0f, -1.0f,  1.0f, /* tex coords = */ 0.0f, 0.0f},
-
-	/* Left face */
-	{ /* pos = */ -1.0f, -1.0f, -1.0f, /* tex coords = */ 0.0f, 0.0f},
-	{ /* pos = */ -1.0f, -1.0f,  1.0f, /* tex coords = */ 1.0f, 0.0f},
-	{ /* pos = */ -1.0f,  1.0f,  1.0f, /* tex coords = */ 1.0f, 1.0f},
-	{ /* pos = */ -1.0f,  1.0f, -1.0f, /* tex coords = */ 0.0f, 1.0f}
 };
 
 static void paint_cb (struct pe *pe) {
@@ -94,11 +46,10 @@ main(int argc, char **argv)
 	CoglError *error = NULL;
 	struct pe pe;
 	float fovy, aspect, z_near, z_2d, z_far;
-	CoglDepthState depth_state;
 	unsigned int i;
 
 	ctx = cogl_context_new (NULL, &error);
-	if (!ctx)
+	if (!ctx || error != NULL)
 		g_error("Failed to create Cogl context\n");
 
 	onscreen = cogl_onscreen_new(ctx, WIDTH, HEIGHT);
@@ -106,7 +57,6 @@ main(int argc, char **argv)
 	pe.fb = COGL_FRAMEBUFFER(onscreen);
 	pe.width = cogl_framebuffer_get_width(pe.fb);
 	pe.height = cogl_framebuffer_get_height(pe.fb);
-	pe.timer = g_timer_new();
 
 	cogl_onscreen_show(onscreen);
 	cogl_framebuffer_set_viewport(pe.fb, 0, 0, pe.width, pe.height);
@@ -122,33 +72,6 @@ main(int argc, char **argv)
 	cogl_matrix_view_2d_in_perspective(&pe.view, fovy, aspect, z_near, z_2d,
 					   pe.width, pe.height);
 	cogl_framebuffer_set_modelview_matrix(pe.fb, &pe.view);
-
-	/* Convenient constants: */
-	cogl_matrix_init_identity(&identity);
-	cogl_color_init_from_4ub(&white, 0xff, 0xff, 0xff, 0xff);
-
-	pe.indices = cogl_get_rectangle_indices(ctx, 6);
-	pe.primitive = cogl_primitive_new_p3t2(ctx,
-					       COGL_VERTICES_MODE_TRIANGLES,
-					       G_N_ELEMENTS(vertices),
-					       vertices);
-	cogl_primitive_set_indices (pe.primitive, pe.indices, 6 * 6);
-
-	pe.texture = cogl_texture_new_from_file (ctx,
-						 "assets/img/cube-texture.jpg",
-						 COGL_TEXTURE_NO_SLICING,
-						 COGL_PIXEL_FORMAT_ANY,
-						 &error);
-
-	if (!pe.texture)
-		g_error("Failed to load texture");
-
-	pe.cube_pipeline = cogl_pipeline_new(ctx);
-	cogl_pipeline_set_layer_texture(pe.cube_pipeline, 0, pe.texture);
-
-	cogl_depth_state_init(&depth_state);
-	cogl_depth_state_set_test_enabled(&depth_state, TRUE);
-	cogl_pipeline_set_depth_state(pe.cube_pipeline, &depth_state, NULL);
 	pe.swap_ready = TRUE;
 
 	cogl_onscreen_add_frame_callback(COGL_ONSCREEN(pe.fb),
