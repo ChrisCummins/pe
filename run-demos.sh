3f22d460a2b7c222593504e47ace41281f9eecf4
Chris Cummins <christopher.e.cummins@intel.com>
2013-06-13 11:19:54 +0100

    run-demos.sh: Tidy output

--- run-demos.sh
+++ run-demos.sh
@@ -5,11 +5,9 @@ demos=(snow fountains fireworks catherine_wheel)
 time=30
 
 (
-	echo "Beginning ${demos[$?]} demonstrations"
-
 	# Run in sub-shell to suppress stderr garbage from kill
 	for d in ${demos[*]}; do
-		echo "Running demonstration ./$d for $time seconds..."
+		echo "./$d for $time seconds..."
 		(cmdpid=$BASHPID; (sleep $time; kill $cmdpid) & exec ./$d)
 	done
 
