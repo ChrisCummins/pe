f03c12b416e05c9e66c87199cd7a6fa080953f26
Chris Cummins <christopher.e.cummins@intel.com>
2013-07-25 12:58:02 +0100

    web/boids.js: Minor style fixes

--- web/boids.js
+++ web/boids.js
@@ -55,7 +55,7 @@ var Boids = Boids || {};
       los: 300,
       /* The rate at which boids can manoeuvre (higher value means more agile
        * boids): */
-      turnRate: 0.1,
+      turnRate: 0.1
     },
 
     MOUSE: {
@@ -381,8 +381,8 @@ var Boids = Boids || {};
 
         /* Apply the velocity change */
         var velocityChange = new THREE.Vector3().subVectors(
-          b.velocity.clone().add(dv),
-          b.velocity);
+            b.velocity.clone().add(dv),
+            b.velocity);
 
         b.velocity.add(velocityChange.multiplyScalar(b.speed *
                                                      config.BOIDS.turnRate));
